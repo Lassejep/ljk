247dadadef636a58911b2d536fc5ca0e12a77db4
lassejep <lasse.jepsen@hotmail.com>
2024-05-08 15:58:04 +0200

    removed unnecessary files

--- src/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, user):
-        self.id = user["id"]
-        self.email = user["email"]
-        self.salt = user["salt"]
-        self.auth_key = user["auth_key"]
-        self.vaults = []
-        self.master_pass = None
