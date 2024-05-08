e5cece6d3694c1f72d7ae09dcee6ad847addbf0a
lassejep <lasse.jepsen@hotmail.com>
2024-05-08 15:52:11 +0200

    updated test_handlers to work for async handlers

--- test/test_handlers.py
+++ test/test_handlers.py
@@ -1,15 +1,21 @@
 import unittest
 import os
+import websockets
+import ssl
 from src import encryption, handlers
-from websockets.sync.client import connect
 
 
-class TestClient(unittest.TestCase):
-    def setUp(self):
+class TestClient(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
         self.db_path = "/tmp/test.db"
         if os.path.exists(self.db_path):
             os.remove(self.db_path)
-        self.ws = connect("ws://localhost:8765")
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        localhost_pem = "/home/tinspring/ws/ljk/localhost.pem"
+        ssl_context.load_verify_locations(localhost_pem)
+        self.ws = await websockets.connect(
+            "wss://localhost:8765", ssl=ssl_context, ping_interval=None
+        )
         self.email = "email"
         self.mkey = "mkey"
         self.vault_name = "vault_name"
@@ -20,104 +26,106 @@ class TestClient(unittest.TestCase):
         self.password = "test_password"
         self.notes = "test_notes"
 
-    def test_register(self):
-        response = handlers.register(self.ws, self.email, self.mkey)
+    async def test_register(self):
+        response = await handlers.register(self.ws, self.email, self.mkey)
         self.assertEqual(response, True)
 
-    def test_login(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
+    async def test_login(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
         self.assertIsNotNone(user)
 
-    def test_get_vaults(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
-        vaults = handlers.get_vaults(self.ws, user)
+    async def test_get_vaults(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+        vaults = await handlers.get_vaults(self.ws, user)
         self.assertEqual(len(vaults), 1)
         self.assertEqual(vaults[0]["name"], self.vault_name)
 
-    def test_get_vault(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
-        vault = handlers.get_vault(self.ws, user, self.vault_name, self.mkey)
+    async def test_get_vault(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+        vault = await handlers.get_vault(
+            self.ws, user, self.vault_name, self.mkey
+        )
         self.assertIsNotNone(vault)
         self.assertIsNotNone(vault.connection)
 
-    def test_save_vault(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
-        self.vault = handlers.get_vault(
+    async def test_save_vault(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+        self.vault = await handlers.get_vault(
             self.ws, user, self.vault_name, self.mkey
         )
         self.vault.add(self.service, self.user, self.password, self.notes)
-        save = handlers.save_vault(self.ws, user, self.vault)
+        save = await handlers.save_vault(self.ws, user, self.vault)
         self.assertTrue(save)
 
-    def test_create_vault(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+    async def test_create_vault(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
         new_vault_name = "new_vault"
-        handlers.create_vault(self.ws, user, new_vault_name, self.mkey)
-        vaults = handlers.get_vaults(self.ws, user)
+        await handlers.create_vault(self.ws, user, new_vault_name, self.mkey)
+        vaults = await handlers.get_vaults(self.ws, user)
         self.assertEqual(len(vaults), 2)
         self.assertNotEqual(vaults[0]["name"], new_vault_name)
         self.assertEqual(vaults[1]["name"], new_vault_name)
 
-    def test_delete_vault(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
-        vaults = handlers.get_vaults(self.ws, user)
+    async def test_delete_vault(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+        vaults = await handlers.get_vaults(self.ws, user)
         self.assertTrue(len(vaults) > 0)
-        handlers.delete_vault(self.ws, user, self.vault_name)
-        vaults = handlers.get_vaults(self.ws, user)
+        await handlers.delete_vault(self.ws, user, self.vault_name)
+        vaults = await handlers.get_vaults(self.ws, user)
         self.assertEqual(len(vaults), 0)
 
-    def test_change_master_pass(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
-        self.vault = handlers.get_vault(
+    async def test_change_master_pass(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+        self.vault = await handlers.get_vault(
             self.ws, user, self.vault_name, self.mkey
         )
         new_mkey = "new_master_pass"
         self.vault.add(self.service, self.user, self.password, self.notes)
-        status = handlers.change_mkey(self.ws, user, self.mkey, new_mkey)
+        status = await handlers.change_mkey(self.ws, user, self.mkey, new_mkey)
         self.assertTrue(status)
-        user = handlers.auth(self.ws, self.email, new_mkey)
+        user = await handlers.auth(self.ws, self.email, new_mkey)
         self.assertIsNotNone(user)
 
-    def test_change_email(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
+    async def test_change_email(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
         new_email = "new_email"
-        status = handlers.change_email(self.ws, user, new_email)
+        status = await handlers.change_email(self.ws, user, new_email)
         self.assertTrue(status)
-        user = handlers.auth(self.ws, new_email, self.mkey)
+        user = await handlers.auth(self.ws, new_email, self.mkey)
         self.assertIsNotNone(user)
 
-    def test_delete_account(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.delete_account(self.ws, user)
-        user = handlers.auth(self.ws, self.email, self.mkey)
+    async def test_delete_account(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.delete_account(self.ws, user)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
         self.assertIsNone(user)
 
-    def test_update_vault_key(self):
-        handlers.register(self.ws, self.email, self.mkey)
-        user = handlers.auth(self.ws, self.email, self.mkey)
-        handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
+    async def test_update_vault_key(self):
+        await handlers.register(self.ws, self.email, self.mkey)
+        user = await handlers.auth(self.ws, self.email, self.mkey)
+        await handlers.create_vault(self.ws, user, self.vault_name, self.mkey)
         new_vkey = encryption.generate_vault_key()
-        status = handlers.update_vault_key(
+        status = await handlers.update_vault_key(
             self.ws, user, self.vault_name, new_vkey
         )
         self.assertTrue(status)
 
-    def tearDown(self):
-        self.ws.close()
+    async def asyncTearDown(self):
+        await self.ws.close()
         if self.vault is not None:
             self.vault.rm()
